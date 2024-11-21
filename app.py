1aad2ad547029cbe141d68102a5ac8c8109446dc
vaibhav gawali <vaibhav.gawali1512@gmail.com>
2024-11-22 00:23:14 +0530

    Update in Webpage

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def predict():
     final_input= scaler.transform(np.array(data).reshape(1,-1))
     print(final_input)
     output=model.predict(final_input)[0]
-    return render_template("home.html",prediction_text="The Required Predicted Time is {}".format(output))
+    return render_template("home.html",prediction_text="The Required Predicted Time is {} mins".format(output))
  
 
 if __name__=="__main__":
